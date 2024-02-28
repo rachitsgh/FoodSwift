78a5ffc51c451814a4ffa153350c2354843f2959
rachitsgh <rachit2351@gmail.com>
2024-02-28 12:41:54 +0530

    updated router

---
+++ frontend/src/AppRoutes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Home/Homepage";
+import FoodPage from "./pages/Food/FoodPage";
+const AppRoutes=()=>{
+    return (
+        <Routes>
+            <Route path="/" element={<Homepage/>}/>
+            <Route path="/search/:searchTerm" element={<Homepage/>}/>
+            <Route path="/tags/:tag" element={<Homepage />}/>
+            <Route path="/food/:id" element={<FoodPage/>} />
+        </Routes>
+    )
+}
+
+export default AppRoutes;
