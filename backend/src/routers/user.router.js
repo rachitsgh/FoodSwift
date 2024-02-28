a078205653f9d486434704898f7e4e17a4f9be73
Prateek <prateek2344.be21@chitkara.edu.in>
2024-02-28 23:14:05 +0530

    initial commit

---
+++ backend/src/routers/user.router.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import {sample_users} from '../data.js';
+import jwt from 'jsonwebtoken;'
+
+const router=Router();
+
+router.post('/login',(req,res) =>{
+    const {email,password} = req.body;
+
+    const user=sample_users.find(
+        user=> user.email=== email && user.password===password
+    );
+    
+    if(user){
+
+    }
+});
+
+const gener
