9184b38dc52b901d6b044a719108899bf6527fd2
rachitsgh <rachit2351@gmail.com>
2024-03-09 09:14:59 +0530

    food page updated

--- frontend/src/pages/Food/FoodPage.js
+++ frontend/src/pages/Food/FoodPage.js
@@ -4,7 +4,8 @@ import Price from '../../components/Price/Price';
 import StarRating from '../../components/StarRating/StarRating';
 import Tags from '../../components/Tags/Tags';
 import { useCart } from '../../hooks/useCart';
-import { getById } from '../../services/foodService';
+// import { getById } from '../../services/foodService';
+import { getById } from '../../Services/foodService';
 import classes from './foodPage.module.css';
 import NotFound from '../../components/NotFound/NotFound';
 export default function FoodPage() {
