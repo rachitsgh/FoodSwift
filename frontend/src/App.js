4e43f5d82ac97cbe25e44debf43610cfeedb785c
Prateek <prateek2344.be21@chitkara.edu.in>
2024-03-18 01:22:47 +0530

    finishing touches

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
-import Header from "./components/Header/Header";
-import AppRoutes from "./AppRoutes"
-import React from "react";
+import AppRoutes from './AppRoutes';
+import Header from './components/Header/Header';
+import Loading from './components/Loading/Loading';
+import { useLoading } from './hooks/useLoading';
+import { setLoadingInterceptor } from './interceptors/loadingInterceptor';
+import { useEffect } from 'react';
 
 function App() {
+  const { showLoading, hideLoading } = useLoading();
+
+  useEffect(() => {
+    setLoadingInterceptor({ showLoading, hideLoading });
+  }, []);
+
   return (
     <>
-    <Header/>
-    <AppRoutes/>
+      <Loading />
+      <Header />
+      <AppRoutes />
     </>
   );
 }
