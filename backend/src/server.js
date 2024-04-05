67e4063a599d9e05b3dba671150255e53fb26f0e
Prateek <prateek2344.be21@chitkara.edu.in>
2024-04-05 17:26:40 +0530

    deploying

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,21 +29,16 @@ app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 app.use('/api/upload', uploadRouter);
 
-const publicFolder = path.join(__dirname, 'public');
-app.use(express.static(publicFolder));
 
+const frontendPath = path.join(__dirname, '../frontend/build');
+
+// Serve the static files from the frontend directory
+app.use(express.static(frontendPath));
+
+// Route to serve index.html for all routes
 app.get('*', (req, res) => {
-  const indexFilePath = path.join(publicFolder, 'index.html');
-  res.sendFile(indexFilePath);
+    res.sendFile(path.join(frontendPath, 'index.html'));
 });
-
-__dirname = path.resolve();
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '..','frontend', 'build', 'index.html'));
-    })
-}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
