c9adc643311f1d64b0b574b33262972adfdb6c69
Prateek <prateek2344.be21@chitkara.edu.in>
2024-04-05 17:22:09 +0530

    deploying

--- backend/src/server.js
+++ backend/src/server.js
@@ -41,7 +41,7 @@ __dirname = path.resolve();
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '/frontend/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, '..','frontend', 'build', 'index.html'));
     })
 }
 
